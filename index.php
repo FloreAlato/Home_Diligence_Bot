eb5af007b0f09577bc274a53b7ba54cabbf7b71f
Davide <davyflore1809@gmail.com>
2022-04-05 22:41:22 +0200

    changes

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $file = fopen("token.bin", "rb");
 $token = fread($file, 46);
 fclose($file);
 
-$url = "https://api.telegram.org/bot" . $token;
+$website = "https://api.telegram.org/bot" . $token;
 
 $content = file_get_contents('php://input');
 
@@ -15,6 +15,16 @@ $update = json_decode($content, true);
 if($update) {
     $chatID = $update["message"]["from"]["id"];
     $message = $update["message"]["text"];
+}
+
+
+function send_message($text) {
+    $url = $website . "/sendMessage?chat_id=" . $me . "?text=" . $text;
+}
+
+
+if($message == "oi") {
+    send_message("Fatto");
 }
 
 
